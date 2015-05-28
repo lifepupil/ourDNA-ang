272c1f687c25819b711310f11ec6c07afb091357
Christian Richard <lifepupil@gmail.com>
2015-05-28 16:16:36 -0700

    working genotyper. fix of chromosome double saves. added buggy snpedia rs call

--- client/views/general/home.js
+++ client/views/general/home.js
@@ -3,10 +3,41 @@
 angular.module('ourDna')
 .controller('homeCtrl', function($scope, Person){
   $scope.rawtxt = '';
+  $scope.snpResults;
+
+
+  $scope.getGenotype = function(snp_id){
+    console.log('inside the controller get getGenotype');
+    Person.GET_Genotype(snp_id)
+    .then(function(response){
+      var t = response.data[0].chrom[0].genotype;
+      var snpArr = [];
+      console.log('rs info returned', t);
+
+      var snpObj = {};
+
+      for(var i = 0; i < response.data.length; i++){
+        snpObj = {
+          firstName: response.data[i].givenName,
+          lastName: response.data[i].surName,
+          genotype: response.data[i].chrom[0].genotype,
+          sex: response.data[i].sex,
+          chromosomeId: response.data[i].chromosomeId
+        }
+        snpArr.push(snpObj);
+      }
+
+      $scope.snpResults = snpArr;
+      // var str = 'CLICK HERE'
+      // var x = "Rs4680";
+      // var result = str.link("http://www.snpedia.com/index.php/"+ x);
+      // document.getElementById("demo").innerHTML = result;
+    });
+  }
 
   $scope.getDNAfile = function(dnafile){
     var theFile = dnafile.files[0];
-    console.log('dnafile name', theFile.name);
+    // console.log('dnafile name', theFile.name);
     $scope.theFileName = theFile.name;
     // var reader = new FileReader();
     // reader.onloadend = function(loadEvent){
@@ -39,8 +70,9 @@ angular.module('ourDna')
     console.log('person: ', personObj);
     Person.addPerson(personObj);
 
-    $('#dnaHome').val('');
-    profile.givenName = '';
-    profile.surName = '';
+    // $('#dnaHome').val('');
+    // profile.givenName = '';
+    // profile.surName = '';
+    // profile.sex = '';
   };
 });
