b5446738f6f251307f60aec3875120f10cb25191
Christian Richard <lifepupil@gmail.com>
2015-05-30 11:17:42 -0700

    can scale chromosome view to render on x-axis with chromosome position vals

--- client/views/general/home.js
+++ client/views/general/home.js
@@ -8,88 +8,49 @@ angular.module('ourDna')
 
 
   d3.select("body").style("background-color", "grey");
-  // d3.selectAll("p")
-  //     .data([4, 8, 15, 16, 23, 42, 50])
-  //     .style("font-size", function(d) { return d + "px"; });
-  //
-  // var section = d3.selectAll('p');
-  // var div = section.append('div');
-  // div.html('Hello, world!');
 
-  // var data = [4, 8, 15, 16, 23, 42, 50, 100000];
-  // var x = d3.scale.linear()
-  //   .domain([0, d3.max(data)])
-  //   .range([0, 420]);
-  //
-  // d3.select(".chart")
-  // .selectAll("div")
-  //   .data(data)
-  // .enter().append("div")
-  //   .style("width", function(d) { return x(d) + "px"; })
-  //   .text(function(d) { return d; });
-
-
-  // var dataset = [4, 8, 15, 16, 23, 42, 79, 41, 12, 44, 23];
   var dataset = [
-                  [ 5,     20 ],
-                  [ 580,   60 ],
-                  [ 780,   20 ],
-                  [ 880,   60 ],
-                  [ 250,   50 ],
-                  [ 330,   95 ],
-                  [ 410,   12 ],
-                  [ 475,   44 ],
-                  [ 45,    40 ],
-                  [ 105,    31 ],
-                  [ 220,   88 ]
+                  [ 50,     20 ],
+                  [ 450,    41 ],
+                  [ 1050,   32 ],
+                  [ 2200,   13 ],
+                  [ 2500,   54 ],
+                  [ 3300,   75 ],
+                  [ 4100,   16 ],
+                  [ 4750,   67 ],
+                  [ 5800,   68 ],
+                  [ 7800,   29 ],
+                  [ 8800,   60 ],
+                  [ 11880,   61 ],
+                  [ 18000,   1000 ]
               ];
-  var w = 1200;
+  var w = 1100;
   var h = 200;
-  var svg = d3.select(".chart")
+  var svg = d3.select(".chromosomeView")
       .append("svg")
       .attr("width", w)
       .attr("height", h);
-  // var data = [4, 8, 15, 16, 23, 42];
-  // var chrLength = 100;
-  // var width = 550,
-  //     barHeight = 20;
-  // var x = d3.scale.linear()
-  //     .domain([0, chrLength])
-  //     .range([0, width]);
-  // var chart = d3.select(".chart")
-  //     .attr("width", width)
-  //     .attr("height", barHeight * data.length);
-  // var bar = chart.selectAll("g")
-  //     .data(data)
-  //   .enter().append("g")
-  //     .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-  // bar.append("rect")
-  //     .attr("width", x)
-  //     .attr("height", barHeight - 1);
-  // bar.append("text")
-  //     .attr("x", function(d) { return x(d) - 3; })
-  //     .attr("y", barHeight / 2)
-  //     .attr("dy", ".35em")
-  //     .text(function(d) { return d; });
 
+  var xScale = d3.scale.linear()
+                       .domain([0, 25000])
+                       .range([0, w]);
 
   svg.selectAll("rect")
      .data(dataset)
      .enter()
      .append("rect")
      .attr("x", function(d, i){
-
-       return d[0];
-      //  return i * (w / dataset.length);
-      //  return i * (w / dataset.length);
+       return xScale(d[0]);
      })
      .attr("y", 1)
-     .attr("width", function(d, i){
-       return d[1];
-      //  return d * Math.random();
-     })
+     .attr("width", function(d, i){ return d[1]; })
+     .attr("width", function(d, i){ return xScale(d[1]); })
      .attr("height", 100)
      .text('hi');
+
+
+
+
 
 
 
