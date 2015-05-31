733349c767e9395e8c4a30e21712ba99024dab1c
Christian Richard <lifepupil@gmail.com>
2015-05-30 18:40:08 -0700

    chromosome view changes based on actual length and centromere position

--- client/views/general/home.js
+++ client/views/general/home.js
@@ -7,67 +7,34 @@ angular.module('ourDna')
   $scope.snpResultShow = false;
 
 
-  d3.select("body").style("background-color", "grey");
-  var chromosomeLength = 26000000;
-  var p_endPos = 6000000;
-  var q_endPos = chromosomeLength - p_endPos;
-  var chomosomeArms = [
-    [0, p_endPos],
-    [p_endPos + 1, q_endPos]
+  // d3.select("body").style("background-color", "grey");
+  var chromosomeInfo = [
+    ['1', 249250621, 125.0],
+    ['2', 243199373, 93.3],
+    ['3', 243199373, 91.0],
+    ['4', 191154276, 50.4],
+    ['5', 180915260, 48.4],
+    ['6', 171115067, 61.0],
+    ['7', 159138663, 59.9],
+    ['8', 146364022, 45.6],
+    ['9', 141213431, 49.0],
+    ['10', 135534747, 40.2],
+    ['11', 135006516, 53.72],
+    ['12', 133851895, 35.8],
+    ['13', 115169878, 17.9],
+    ['14', 1073495408, 17.6],
+    ['15', 1025313928, 19.6],
+    ['16', 90354753, 36.6],
+    ['17', 81195210, 24.0],
+    ['18', 78077248, 17.20],
+    ['19', 59128983, 26.5],
+    ['20', 63025520, 27.5],
+    ['21', 48129895, 13.2],
+    ['22', 51304566, 14.7],
+    ['X', 155270560, 60.6],
+    ['Y', 59373566, 12.5],
+    ['mtDNA', 16569, 0]
   ];
-
-  var dataset = [
-                  [ 50000,     20000 ],
-                  [ 450000,    41000 ],
-                  [ 1050000,   32000 ],
-                  [ 2200000,   13000 ],
-                  [ 2500000,   54000 ],
-                  [ 3300000,   75000 ],
-                  [ 4100000,   16000 ],
-                  [ 4750000,   67000 ],
-                  [ 5800000,   68000 ],
-                  [ 7800000,   29000 ],
-                  [ 8800000,   60000 ],
-                  [ 11880000,   61000 ],
-                  [ 25000000,   600000 ]
-              ];
-  var w = 1100;
-  var h = 100;
-  var svg = d3.select(".chromosomeView")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
-
-  var xScale = d3.scale.linear()
-     .domain([0, chromosomeLength])
-     .range([0, w]);
-
-  var ctGroup = svg.append('g');
-  var snpGroup = svg.append('g');
-
-  ctGroup.selectAll("chromosomeTails")
-     .data(chomosomeArms)
-    .enter()
-     .append("rect")
-       .attr("x", function(d, i){ return xScale(d[0]); })
-       .attr("y", 1)
-       .attr('rx', 20)
-       .attr('ry', 20)
-       .attr("width", function(d, i){ return xScale(d[1]); })
-       .attr("height", h - 2)
-       .style('fill', 'white')
-       .style('stroke', 'black')
-       .style('stroke-width', 5)
-
-  snpGroup.selectAll("snps")
-     .data(dataset)
-    .enter()
-     .append("rect")
-       .attr("x", function(d, i){ return xScale(d[0]); })
-       .attr("y", 1)
-       .attr("width", function(d, i){ return xScale(d[1]); })
-       .attr("height", h - 2)
-       .style('fill', 'red')
 
 
 
@@ -92,6 +59,7 @@ angular.module('ourDna')
           firstName: response.data[i].givenName,
           lastName: response.data[i].surName,
           genotype: response.data[i].chrom[0].genotype,
+          position: response.data[i].chrom[0].position,
           sex: response.data[i].sex,
           chromosomeId: response.data[i].chromosomeId
         }
@@ -99,10 +67,59 @@ angular.module('ourDna')
       }
 
       $scope.snpResults = snpArr;
-      var str = 'CLICK HERE'
-      var x = "Rs4680";
-      var result = str.link("http://www.snpedia.com/index.php/"+ x);
-      document.getElementById("demo").innerHTML = result;
+      var chromIndex = 0;
+      for(var i = 0; i < chromosomeInfo.length; i++){
+        if(chromosomeInfo[i][0] === $scope.snpResults[0].chromosomeId){
+          chromIndex = i;
+        }
+      }
+
+      var chromosomeLength = chromosomeInfo[chromIndex][1];
+      var p_endPos = chromosomeInfo[chromIndex][2] * 1000000;
+      var q_endPos = chromosomeLength - p_endPos;
+      var chomosomeArms = [
+        [0, p_endPos],
+        [p_endPos + 1, q_endPos]
+      ];
+
+      var dataset = [[$scope.snpResults[0].position, 100000]]
+      var w = 1100;
+      var h = 100;
+      var svg = d3.select(".chromosomeView")
+          .append("svg")
+          .attr("width", w)
+          .attr("height", h);
+
+      var xScale = d3.scale.linear()
+         .domain([0, chromosomeLength])
+         .range([0, w]);
+
+      var ctGroup = svg.append('g');
+      var snpGroup = svg.append('g');
+
+      ctGroup.selectAll("chromosomeTails")
+         .data(chomosomeArms)
+        .enter()
+         .append("rect")
+           .attr("x", function(d, i){ return xScale(d[0]); })
+           .attr("y", 1)
+           .attr('rx', 20)
+           .attr('ry', 20)
+           .attr("width", function(d, i){ return xScale(d[1]); })
+           .attr("height", h - 2)
+           .style('fill', 'white')
+           .style('stroke', 'black')
+           .style('stroke-width', 5)
+
+      snpGroup.selectAll("snps")
+         .data(dataset)
+        .enter()
+         .append("rect")
+           .attr("x", function(d, i){ return xScale(d[0]); })
+           .attr("y", 1)
+           .attr("width", function(d, i){ return xScale(d[1]); })
+           .attr("height", h - 2)
+           .style('fill', 'red')
     });
   }
 
