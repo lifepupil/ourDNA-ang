3a02006f5ea97c7c8d5579cb495759b2ca9d8a9a
Christian Richard <lifepupil@gmail.com>
2015-06-04 11:39:27 -0700

    preparing for upload to heroku and amazon - take2

--- client/config/constants.js
+++ client/config/constants.js
@@ -2,5 +2,5 @@
 
 angular.module('ourDna')
 .constant('firebaseUrl', 'https://facebook-cdr.firebaseio.com/')
-.constant('nodeUrl', 'https://lit-castle-1834.herokuapp.com');
+.constant('nodeUrl', 'https://lit-bayou-6188.herokuapp.com');
 // .constant('nodeUrl', 'http://localhost:8000');
